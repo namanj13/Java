4814744a512e4b56d8179b5da0f963553062f624
Naman Jain <133277339+namanj13@users.noreply.github.com>
2025-09-24 12:21:58 +0530

    Create pyramid.java

---
+++ Pattern-1/pyramid.java
@@ -0,0 +1,11 @@
+public class pyramid {   //HALF-PYRAMID
+    public static void main(String args[]){
+        int n =4;
+        for(int lines =1;lines<=n;lines++){
+            for(int num =1; num<=lines;num++){
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+}
