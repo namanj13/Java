485d45e673fdbbb7f1350621f00e1a695efbbf45
Naman Jain <133277339+namanj13@users.noreply.github.com>
2025-09-24 12:22:26 +0530

    Create star.java

---
+++ Pattern-1/star.java
@@ -0,0 +1,11 @@
+public class star {
+    public static void main(String args[]){
+        for(int lines = 1; lines<=4;lines++){
+            // one line
+            for(int star = 1; star <= lines; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
