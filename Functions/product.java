ffab069282d94e277954d188ea3d08a372230fb5
Naman Jain <133277339+namanj13@users.noreply.github.com>
2025-09-28 20:49:58 +0530

    Create product.java

---
+++ Functions/product.java
@@ -0,0 +1,15 @@
+public class product {
+
+    public static int multiply(int a , int b){
+        int product = a*b;
+        return product;
+    }
+    public static void main( String args[]){
+        int a = 2;
+        int b = 8;
+        int prod = multiply(a, b);
+        System.out.println("a*b :" + prod); 
+        prod = multiply(10,25);
+        System.out.println("a*b :" + prod);
+    }
+}
